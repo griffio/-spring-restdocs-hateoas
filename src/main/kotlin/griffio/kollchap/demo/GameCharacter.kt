f72ae3815a9dc0713ebb0c670a910fb31ec37093
griffio <griffio@users.noreply.github.com>
2022-07-20 18:53:47 +0100

    Add extra character attributes
    All are required

--- src/main/kotlin/griffio/kollchap/demo/GameCharacter.kt
+++ src/main/kotlin/griffio/kollchap/demo/GameCharacter.kt
@@ -4,10 +4,19 @@ import com.fasterxml.jackson.annotation.JsonIgnore
 import org.springframework.data.jpa.domain.AbstractPersistable
 import javax.persistence.Entity
 import javax.validation.constraints.NotNull
-
+enum class GameCharacterAlignment {
+    Chaotic,
+    Lawful,
+    Neutral
+}
 @Entity(name = "character")
 class GameCharacter(
-    @get:NotNull val name: String, val background: String
+    @get:NotNull val name: String,
+    @get:NotNull val background: String,
+    @get:NotNull val level: Int,
+    @get:NotNull val armourClass: Int,
+    @get:NotNull val hitPoints: Int,
+    @get:NotNull val alignment: GameCharacterAlignment
 ) : AbstractPersistable<Long>() {
     @JsonIgnore
     override fun getId(): Long? {
