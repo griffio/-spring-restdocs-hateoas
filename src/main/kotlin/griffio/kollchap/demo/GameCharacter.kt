32de6fe9b608ac659adc573c5ac22612c93a5b69
griffio <griffio@users.noreply.github.com>
2022-07-21 10:22:41 +0100

    Add range validation to character level
    Hibernate validator range
    
    Level can be 1 to 20

--- src/main/kotlin/griffio/kollchap/demo/GameCharacter.kt
+++ src/main/kotlin/griffio/kollchap/demo/GameCharacter.kt
@@ -3,7 +3,12 @@ package griffio.kollchap.demo
 import com.fasterxml.jackson.annotation.JsonIgnore
 import org.springframework.data.jpa.domain.AbstractPersistable
 import javax.persistence.Entity
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
+
+import org.hibernate.validator.constraints.Range
 enum class GameCharacterAlignment {
     Chaotic,
     Lawful,
@@ -11,11 +16,11 @@ enum class GameCharacterAlignment {
 }
 @Entity(name = "character")
 class GameCharacter(
-    @get:NotNull val name: String,
-    @get:NotNull val background: String,
-    @get:NotNull val level: Int,
-    @get:NotNull val armourClass: Int,
-    @get:NotNull val hitPoints: Int,
+    @get:NotBlank val name: String,
+    @get:NotBlank val background: String,
+    @get:Range(min = 1, max = 20) val level: Int,
+    @get:Min(0) val armourClass: Int,
+    @get:Min(1) val hitPoints: Int,
     @get:NotNull val alignment: GameCharacterAlignment
 ) : AbstractPersistable<Long>() {
     @JsonIgnore
