c647cfe160e6b4175cc73ea1d4489629c29f4021
griffio <griffio@users.noreply.github.com>
2022-07-27 18:02:06 +0100

    Add room with mappedIdentity

--- src/main/kotlin/griffio/kollchap/demo/DungeonRoom.kt
+++ src/main/kotlin/griffio/kollchap/demo/DungeonRoom.kt
@@ -1,15 +1,14 @@
 package griffio.kollchap.demo
 
 import com.fasterxml.jackson.annotation.JsonIgnore
-import org.springframework.data.jpa.domain.AbstractPersistable
 import javax.persistence.Entity
 import javax.validation.constraints.NotBlank
 
-@Entity(name = "location")
-class DungeonLocation(
+@Entity(name = "room")
+class DungeonRoom(
     @get:NotBlank val name: String,
     @get:NotBlank val description: String,
-) : AbstractPersistable<Long>() {
+) : MappedIdentity<Long>() {
     @JsonIgnore
     override fun getId(): Long? = super.getId()
 
