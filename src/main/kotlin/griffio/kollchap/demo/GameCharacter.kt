579ac459fe6eb16af86681248e9f76715058f794
griffio <griffio@users.noreply.github.com>
2022-07-29 14:04:45 +0100

    Add GameCharacterStats
    Six basic DnD characteristics
    
    GameCharacter

--- src/main/kotlin/griffio/kollchap/demo/GameCharacter.kt
+++ src/main/kotlin/griffio/kollchap/demo/GameCharacter.kt
@@ -7,6 +7,8 @@ import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
 
 import org.hibernate.validator.constraints.Range
+import javax.persistence.Embeddable
+import javax.persistence.Embedded
 
 enum class GameCharacterAlignment {
     Chaotic,
@@ -38,10 +40,34 @@ class GameCharacter(
     @get:Range(min = 1, max = 20) val level: Int,
     @get:Range(min = 0, max = 9) val armourClass: Int,
     @get:Min(0) val hitPoints: Int,
+    @Embedded val characteristics: GameCharacterStats
 ) : MappedIdentity<Long>() {
     @JsonIgnore
     override fun getId(): Long? = super.getId()
 
     @JsonIgnore
     override fun isNew(): Boolean = null == id
+}
+
+@Embeddable
+class GameCharacterStats(
+    @get:Range(min = 3, max = 18) val str: Int,
+    @get:Range(min = 3, max = 18) val int: Int,
+    @get:Range(min = 3, max = 18) val wis: Int,
+    @get:Range(min = 3, max = 18) val dex: Int,
+    @get:Range(min = 3, max = 18) val con: Int,
+    @get:Range(min = 3, max = 18)val chr: Int
+)
+
+val threeDSix = 3..18
+
+fun rollCharacteristics(): GameCharacterStats {
+    return GameCharacterStats(
+        str = threeDSix.random(),
+        int = threeDSix.random(),
+        wis = threeDSix.random(),
+        dex = threeDSix.random(),
+        con = threeDSix.random(),
+        chr = threeDSix.random()
+    )
 }
