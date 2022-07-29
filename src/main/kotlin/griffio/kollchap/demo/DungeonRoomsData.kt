84fb7c1f6c6427c6b5c50775acf88bf7295f8f52
griffio <griffio@users.noreply.github.com>
2022-07-29 10:19:22 +0100

    Room descriptions

---
+++ src/main/kotlin/griffio/kollchap/demo/DungeonRoomsData.kt
@@ -0,0 +1,28 @@
+package griffio.kollchap.demo
+
+val rooms = listOf(
+    "Orc Guardroom" to "In this room are two sets of bunk beds...In the centre is a large battered table...",
+    "Rubbish Dump" to "This room is used by the inhabitants...It is filled with broken furniture, bones...",
+    "Floorless Room" to "This room was once the entrance to the main part of the shrine...",
+    "Corridor Trap" to "The Slime is clinging to the west wall and will...fall on a party member...",
+    "Storeroom" to "This room was once a storeroom for the temple...some moldy food remains...",
+    "Healing Pool" to "This pool is circular with a radius of about three feet...and has a greenish tint...",
+    "Coffin Storeroom" to "This room is bare except for fifteen plain wooden coffins...stacked in threes...",
+    "Odric's Bedroom" to "This room has a sumptuously furnished bed with silk sheets...",
+    "The Office" to "This room serves as Odric's office...On the table is a mass of papers...",
+    "Collapsed Room" to "The Rockmen are rummaging around in this room...",
+    "UnderPriests' Room" to "This room is used by the three Clerics as a base...",
+    "Room of Absolution" to "This room is almost wholly bare except for six large urns...",
+    "Great Sacrifice Chamber" to "This room is vast...There is a pole in the centre...",
+    "The Cells" to "There are three cells...The cells are all locked...",
+    "Treasure Chamber" to "The door to this room is locked...",
+    "Changing Room" to "This room is used by Odric for changing into his sacrifice robes...",
+    "Old Waiting Room" to "This room was once a waiting room for the oracle...",
+    "Oracle Room" to "This room used to be an oracle room...There is a statue of a seven-foot-high human...",
+    "Pit Trap" to "At this point the floor is covering a pit...The pit used to be a tunnel...",
+    "Goblins' Room" to "This room is very stark, with only straw for bedding, on the floor...",
+    "Cave" to "This room is a cave littered with rocks and boulders...the atmosphere is humid...",
+    "Statue Room" to "In the centre of the room is a statue of a man with his hands held out...",
+    "Meditation Room" to "This room was originally a meditation room...the walls are ornately decorated...",
+    "Flooded Storeroom" to "The stairs leading down are wet and so anyone going down them...",
+)
