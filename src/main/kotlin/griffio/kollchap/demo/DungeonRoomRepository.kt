6cd7a5237b050fd251fd9766b885e4749c0994ba
griffio <griffio@users.noreply.github.com>
2022-08-05 17:19:11 +0100

    Add search by key
    locate room using natural id

--- src/main/kotlin/griffio/kollchap/demo/DungeonRoomRepository.kt
+++ src/main/kotlin/griffio/kollchap/demo/DungeonRoomRepository.kt
@@ -1,9 +1,16 @@
 package griffio.kollchap.demo
 
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.rest.core.annotation.Description
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import org.springframework.data.rest.core.annotation.RestResource
 import org.springframework.stereotype.Component
 
 @Component // annotation enables Intellij to detect bean
 @RepositoryRestResource(collectionResourceRel = "rooms", path = "rooms")
-interface DungeonRoomRepository : CrudRepository<DungeonRoom, String>
+interface DungeonRoomRepository : CrudRepository<DungeonRoom, String> {
+    // All query method resources are exposed under the "/search" resource
+    // can be configured to /rooms/search/keys?key=1.
+    @RestResource(path = "keys", rel = "keys", description = Description("locate room by map key id"))
+    fun findByKey(key: String): List<DungeonRoom>?
+}
